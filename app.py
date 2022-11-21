13d44378544998cdff24cb7330e49ddf0383d372
Daniel <dmssjk@gmail.com>
2022-11-21 17:29:33 -0300

    inicio app.py flask

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template          ##importando o flask  e o render template
+
+app = Flask(__name__)  ## ver video para entender oque isso faz! https://youtu.be/150-dpYG1pg 
+
+
+
+## to pagina tem uma rota e uma funcao, sendo a rota o caminho e a funcao
+@app.route("/") ## rota  ## index 
+def index():    ## funcao
+    return render_template()
+
+@app.route("/principal") ## principal
+def principal():    
+    return render_template()
+
+@app.route("/familia") ## familia
+def familia():    
+    return render_template()
+
+@app.route("/contatos") ## contatos
+def contatos():    
+    return render_template()
+
+@app.route("/trabalhos") ## trabalhos
+def trabalhos():    
+    return render_template()    
+
+
+  
+
+## colocar o site no ar
+if __name__ == "__main__":
+    app.run(debug=True) ## ativar o debug do site ou seja ele exibe automaticamente as alteracoes do codigo no navegador
