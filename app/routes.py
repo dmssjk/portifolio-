43239ed5a0395ff056f73d44e97a495968669cbf
Daniel <dmssjk@gmail.com>
2022-11-29 15:26:37 -0300

    arrumando os extends

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,17 @@ from flask import render_template,url_for
 @app.route('/index')
 
 def index():
-    return render_template('principal.html')
+    return render_template('index.html')
 
 @app.route('/contato')
 def contato():
     return render_template('contato.html') 
+
+@app.route('/familia')
+def familia():
+    return render_template('familia.html') 
+
+@app.route('/trabalhos')
+def trabalhos():
+    return render_template('trabalhos.html') 
+
