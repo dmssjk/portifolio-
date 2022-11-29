932786b4234073d0d2a35f39ef416815b00f8fc5
Daniel <dmssjk@gmail.com>
2022-11-29 11:42:55 -0300

    rotas e inclusao

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,15 @@
 from app import app
 
-from flask import render_template
+from flask import render_template,url_for
+
+
 
 @app.route('/')
 @app.route('/index')
 
 def index():
     return render_template('principal.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html') 
